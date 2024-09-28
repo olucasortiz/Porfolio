17fd098291686a5bc928cb68b9c2076571be100c
olucasortiz <lucassas@outlook.com>
2024-09-28 01:37:51 -0300

    feito

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    const themeSwitch = document.getElementById("theme-switch");
+
+    // Verifica se o tema foi salvo anteriormente
+    if (localStorage.getItem("theme") === "light") {
+        document.body.classList.add("light-theme");
+        themeSwitch.checked = true;
+    }
+
+    themeSwitch.addEventListener("change", function () {
+        if (this.checked) {
+            document.body.classList.add("light-theme");
+            localStorage.setItem("theme", "light");
+        } else {
+            document.body.classList.remove("light-theme");
+            localStorage.setItem("theme", "dark");
+        }
+    });
+
     // Animação de Fade-In
     const projectsTitle = document.getElementById('projects-title');
     const projectsRow = document.getElementById('projects-row');
 
-    projectsTitle.style.opacity = 0;
-    projectsRow.style.opacity = 0;
+    if (projectsTitle && projectsRow) {
+        projectsTitle.style.opacity = 0;
+        projectsRow.style.opacity = 0;
 
-    setTimeout(() => {
-        fadeIn(projectsTitle, 1000);
-        fadeIn(projectsRow, 2000);
-    }, 500);
+        setTimeout(() => {
+            fadeIn(projectsTitle, 1000);
+            fadeIn(projectsRow, 2000);
+        }, 500);
+    }
 
     // Função de Fade-In
     function fadeIn(element, duration) {
@@ -40,5 +60,28 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener('animationend', function() {
             this.classList.remove('animate__animated', 'animate__bounce');
         });
+    });
+
+    // Animação de deslize
+    const slideElements = document.querySelectorAll('.appear');
+
+    const appearOptions = {
+        threshold: 0.1,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('active');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    slideElements.forEach(slideElement => {
+        appearOnScroll.observe(slideElement);
     });
 });
